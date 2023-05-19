8c3f1a5169deaf927bd8a1ff94bd711c104fd31f
Clayton <persinger.clayton@gmail.com>
2023-05-19 14:00:15 -0700

    Init commit

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let vampCreator = this.creator;
+    while (vampCreator !== null) {
+      vampCreator = vampCreator.creator;
+      numberOfVamps++;
+    }
 
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const thisVampSeniority = this.numberOfVampiresFromOriginal;
+    const otherVampSeniority = vampire.numberOfVampiresFromOriginal;
 
+    return thisVampSeniority > otherVampSeniority ?  false : true;
   }
 
   /** Stretch **/
@@ -41,4 +52,3 @@ class Vampire {
 }
 
 module.exports = Vampire;
-
